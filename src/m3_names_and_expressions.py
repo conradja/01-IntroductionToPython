64c2ae7101c74f10bd089bed94ef5d9811fcb461
conradja <conradja@rose-hulman.edu>
2018-11-26 11:03:34 -0500

    finished module

--- src/m3_names_and_expressions.py
+++ src/m3_names_and_expressions.py
@@ -3,7 +3,7 @@ answer = 2 ** 5
 print(answer * 100)
 
 ###############################################################################
-# TODO: 1.
+# DONE: 1.
 #   Read the 2 lines of code ABOVE this _TODO_.  That code:
 #     1. Computes 2 raised to the 5th power,
 #          yielding the object that is the integer 32.
@@ -20,7 +20,7 @@ print(answer * 100)
 ###############################################################################
 
 ###############################################################################
-# TODO: 2.
+# DONE: 2.
 #   Immediately below this _TODO_, write code that:
 #     - Computes 77 plus the cosine of 2.75.
 #         HINT: You will need to import the   math  module (library).
@@ -28,17 +28,19 @@ print(answer * 100)
 #     - Prints the square root of that computed value.
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
 ###############################################################################
-
+import math
+result = 77 + math.cos(2.75)
+print(math.sqrt(result))
 ###############################################################################
-# TODO: 3.
+# DONE: 3.
 #   Immediately below this _TODO_, write code that computes and prints:
 #      the square root of ((41 * 88) + (4 * the cosine of 2))
 #   Use as few or as many intermediate names as you feel appropriate.
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
 ###############################################################################
-
+print(math.sqrt((41*88)+(4*math.cos(2))))
 ###############################################################################
-# TODO: 4.
+# DONE: 4.
 #   Immediately below this _TODO_,
 #   write code that computes the square root of 2 in two ways:
 #     - By using the   math.sqrt   function.
@@ -46,10 +48,11 @@ print(answer * 100)
 #   Print both of the expressions that you write.
 #   Run your code (fix errors as needed), then change the above _TODO_ to DONE.
 ###############################################################################
-
-
+power1 = math.sqrt(2)
+power2 = 2 ** 0.5
+print(power1, power2)
 ###############################################################################
-# TODO: 5.
+# DONE: 5.
 #   Every object has a TYPE and a VALUE.  For example,
 #   for the object that is computed by  math.sqrt(2):
 #      Its TYPE is float  (which is shorthand for "floating point number").
@@ -72,26 +75,29 @@ print(answer * 100)
 #      2. Then write code of the form   print(type(BLAH)).
 #      3. THen RUN the code to LEARN its TYPE.
 
-#       "hello"
-#       'hello'
-#       'a b c'
-#       3 + 3
-#       "3" + "3"
-#       2 ** 100
-#       2.0 ** 100
-#       math.sin(8)
-#       math.sin
-#       print
-#       math
-#       'math'
+#       "hello"  string
+#       'hello'  string
+#       'a b c'  string
+#       3 + 3    int
+#       "3" + "3"  string
+#       2 ** 100  int
+#       2.0 ** 100 float
+#       math.sin(8) float
+#       math.sin    builtin_function_or_method
+#       print       builtin_function_or_method
+#       math        module
+#       'math'      string
 #
 # After you have written and run the code to learn the TYPE
 # of each of the above, change the above _TODO_ to DONE.
 ###############################################################################
+print(type(3.14))
+print(type("hello"), type('hello'), type('a b c'), type(3 + 3), type("3" + "3"), type(2 **100),
+    type(2.0 **100), type(math.sin(8)), type(math.sin), type(print), type(math), type('math'))
 
 ###############################################################################
 #
-# TODO: 6.
+# DONE: 6.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #   Run one more time to be sure that all is still OK.
 #
